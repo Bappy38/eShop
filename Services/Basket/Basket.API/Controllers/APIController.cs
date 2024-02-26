f74adec283f77f1547f026cc56e8748b72a7ff67
Bappy38 <iqbal.bappy.13@gmail.com>
2024-02-26 22:58:21 +0600

    Basket Controller Written and Dockerfile Fixed

---
+++ Services/Basket/Basket.API/Controllers/APIController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers;
+
+[ApiVersion("1")]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+public class APIController : ControllerBase
+{
+}
---
+++ Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,45 @@
+﻿using Basket.Application.Commands;
+using Basket.Application.Queries;
+using Basket.Application.Responses;
+using Basket.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.API.Controllers;
+
+public class BasketController : APIController
+{
+    private readonly IMediator mediator;
+
+    public BasketController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("[action]/{userName}", Name = "GetBasketByUserName")]
+    [ProducesResponseType(typeof(ShoppingCartResponse), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<ShoppingCartResponse>> GetBasket(string userName)
+    {
+        var query = new GetBasketByUserNameQuery(userName);
+        var basket = await mediator.Send(query);
+        return Ok(basket);
+    }
+
+    [HttpPost("CreateBasket")]
+    [ProducesResponseType(typeof(ShoppingCartResponse), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<ShoppingCartResponse>> UpdateBasket([FromBody] CreateBasketCommand createBasketCommand)
+    {
+        var basket = await mediator.Send(createBasketCommand);
+        return Ok(basket);
+    }
+
+    [HttpDelete("DeleteBasketByUserName")]
+    [ProducesResponseType(typeof(int), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> DeleteBasket([FromBody] string userName)
+    {
+        var command = new DeleteBasketByUserNameCommand(userName);
+        return Ok(await mediator.Send(command));
+    }
+}
