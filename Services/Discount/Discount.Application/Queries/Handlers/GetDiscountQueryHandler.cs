8ada6488e80bae6e23000dffee6a1e2c2ba60fbc
Bappy38 <iqbal.bappy.13@gmail.com>
2024-03-15 19:10:58 +0600

    GetDiscountQuery bug fixed at gRPC server

--- Services/Discount/Discount.Application/Queries/Handlers/GetDiscountQueryHandler.cs
+++ Services/Discount/Discount.Application/Queries/Handlers/GetDiscountQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using Discount.Application.Abstractions.Messaging;
+﻿using AutoMapper;
+using Discount.Application.Abstractions.Messaging;
 using Discount.Application.Mappers;
 using Discount.Domain.Repositories;
 using Discount.Grpc.Protos;
@@ -9,20 +10,22 @@ namespace Discount.Application.Queries.Handlers;
 public class GetDiscountQueryHandler : IQueryHandler<GetDiscountQuery, CouponModel>
 {
     private readonly IDiscountRepository discountRepository;
+    private readonly IMapper mapper;
 
-    public GetDiscountQueryHandler(IDiscountRepository discountRepository)
+    public GetDiscountQueryHandler(IDiscountRepository discountRepository, IMapper mapper)
     {
         this.discountRepository = discountRepository;
+        this.mapper = mapper;
     }
     public async Task<CouponModel> Handle(GetDiscountQuery request, CancellationToken cancellationToken)
     {
-        var coupon = discountRepository.GetDiscount(request.productName);
+        var coupon = await discountRepository.GetDiscount(request.productName);
         if (coupon is null)
         {
             throw new RpcException(new Status(StatusCode.NotFound, $"Discount with the product name = {request.productName} not found"));
         }
 
-        var couponModel = DiscountMapper.Mapper.Map<CouponModel>(coupon);
+        var couponModel = mapper.Map<CouponModel>(coupon);
         return couponModel;
     }
 }
