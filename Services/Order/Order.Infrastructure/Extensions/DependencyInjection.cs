7402f0533f8e92ad960853af49efc7e0ca4064c8
Bappy38 <iqbal.bappy.13@gmail.com>
2024-03-17 12:32:17 +0600

    Repositories registered at extension method

---
+++ Services/Order/Order.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Order.Domain.Repositories;
+using Order.Infrastructure.Data;
+using Order.Infrastructure.Repositories;
+
+namespace Order.Infrastructure.Extensions;
+
+public  static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<OrderContext>(options => options.UseSqlServer(
+            configuration.GetConnectionString("OrderConnectionString")));
+        services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        return services;
+    }
+}
