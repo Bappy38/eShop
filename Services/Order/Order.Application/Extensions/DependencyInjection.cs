adb413e245525b873eaf1f54a46e422f70b9061d
Bappy38 <iqbal.bappy.13@gmail.com>
2024-03-17 17:14:54 +0600

    Dependency Injection Extension added for Order Service's Application Layer

---
+++ Services/Order/Order.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Order.Application.Behaviors;
+
+namespace Order.Application.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var applicationAssembly = typeof(DependencyInjection).Assembly;
+
+        services.AddAutoMapper(applicationAssembly);
+        services.AddMediatR(config => config.RegisterServicesFromAssembly(applicationAssembly));
+        services.AddValidatorsFromAssembly(applicationAssembly);
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+
+        return services;
+    }
+}
